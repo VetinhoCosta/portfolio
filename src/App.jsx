98faea4e5bb441428157058f5a50e4e4c313e5c3
VetinhoCosta <112962380+VetinhoCosta@users.noreply.github.com>
2023-11-16 14:16:31 +0000

    attp

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Antonioni",
+  title: "Curso de TSI - IFPB",
+  email: "e1vertoncosta@gmail.com",
+  gitHub: "Vetinhocosta",
+  instagram: "antonioni.everton",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
